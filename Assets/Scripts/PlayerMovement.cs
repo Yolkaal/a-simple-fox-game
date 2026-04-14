7b52e245acf8df85cd9666b65649343cd618a371
Yolkaal <alpharogue71@gmail.com>
2026-04-14 09:11:11 +0200

    feat : déplacement basiques

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,33 +3,36 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private float speed;
-    private Rigidbody2D rb;
+    private Rigidbody rb;
     private float jumpForce;
-    private Transform groundCheck;
-    //private  playerData;
+    [SerializeField]
+    private MovableEntitiesData playerData;
+    public bool isGrounded;
+    public bool canDoubleJump;
+    public bool canFly;
+    public float flySpeed;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        if (Input.GetAxisRaw)
-        {
-            
-        }
-        
+        this.rb = GetComponent<Rigidbody>();
+        speed = playerData.speed;
+        jumpForce = playerData.jumpForce;
+        isGrounded = false;
+        canDoubleJump = playerData.canDoubleJump;
+        canFly = playerData.canFly;
+        flySpeed = playerData.flySpeed;
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        transform.Translate(movement * (speed * Time.deltaTime));
     }
 
-    public bool IsGrounded()
+    public void setGroundedStatus(bool grounded)
     {
-        if(groundCheck==true)
-        {
-            return true;
-        }
-        return false;
+        isGrounded = grounded;
     }
 }
